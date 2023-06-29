841a60a4bd6d489e75732cebbdc08cd9d676370d
Ruslan Makarov <ruslan.makarov@gmail.com>
2023-06-29 22:51:35 +0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <script setup>
+import { NButton, NInput, NSelect } from 'naive-ui'
+
+const options = [
+  {
+    label: "Everybody's Got Something to Hide Except Me and My Monkey",
+    value: 'song0',
+    disabled: true
+  },
+  {
+    label: 'Drive My Car',
+    value: 'song1'
+  },
+  {
+    label: 'Norwegian Wood',
+    value: 'song2'
+  }
+];
 </script>
 
 <template>
-  <button>Button</button>
+  <n-button>naive-ui</n-button>
 
-  <input type="text" value="Input" />
+  <n-input v-model:value="value" type="text" placeholder="Basic Input" />
+    
 
-  <select name="pets" id="pet-select">
-    <option value="">--Please choose an option--</option>
-    <option value="dog">Dog</option>
-    <option value="cat">Cat</option>
-</select>
+  <n-select v-model:value="value" :options="options" />
 </template>
 
 <style scoped>
