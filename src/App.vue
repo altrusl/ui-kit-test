0dde2c3532ddbb85b3799d63dca7692f43f9136d
Ruslan Makarov <ruslan.makarov@gmail.com>
2023-06-29 22:46:10 +0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <script setup>
+import { Button } from 'vant';
+import { Field } from 'vant';
+import { DropdownMenu, DropdownItem } from 'vant';
+
+const option1 = [
+      { text: 'Option1', value: 0 },
+      { text: 'Option2', value: 1 },
+      { text: 'Option3', value: 2 },
+    ];
+
 </script>
 
 <template>
-  <button>Button</button>
+  <Button type="primary" >Button</Button>
 
-  <input type="text" value="Input" />
+  <Field v-model="value" label="Label" placeholder="Text" />
 
-  <select name="pets" id="pet-select">
-    <option value="">--Please choose an option--</option>
-    <option value="dog">Dog</option>
-    <option value="cat">Cat</option>
-</select>
+  <dropdown-menu>
+  <dropdown-item v-model="value1" :options="option1" />
+</dropdown-menu>
 </template>
 
 <style scoped>
