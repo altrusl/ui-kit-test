9354390d6b5ddb0d6844c0f64ddff6b19450da3e
Ruslan Makarov <ruslan.makarov@gmail.com>
2023-06-29 22:20:02 +0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <script setup>
+import Button from "primevue/button"
+import InputText from 'primevue/inputtext';
+import Dropdown from 'primevue/dropdown';
+
+import { ref } from "vue";
+
+const selectedCity = ref();
+const cities = ref([
+    { name: 'New York', code: 'NY' },
+    { name: 'Rome', code: 'RM' },
+    { name: 'London', code: 'LDN' },
+    { name: 'Istanbul', code: 'IST' },
+    { name: 'Paris', code: 'PRS' }
+]);
+
+
 </script>
 
 <template>
-  <button>Button</button>
+  <Button label="Submit" />
 
-  <input type="text" value="Input" />
+  <InputText type="text" v-model="value" />
 
-  <select name="pets" id="pet-select">
-    <option value="">--Please choose an option--</option>
-    <option value="dog">Dog</option>
-    <option value="cat">Cat</option>
-</select>
+  <Dropdown v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select a City" class="w-full md:w-14rem" />
+
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+import "primevue/resources/primevue.min.css";
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(PrimeVue)
+    .mount('#app')
