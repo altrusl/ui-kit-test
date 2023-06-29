485bbcc887b46125e0f26383b4859896ee3098b3
Ruslan Makarov <ruslan.makarov@gmail.com>
2023-06-29 21:44:28 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <button>Button</button>
+
+  <input type="text" value="Input" />
+
+  <select name="pets" id="pet-select">
+    <option value="">--Please choose an option--</option>
+    <option value="dog">Dog</option>
+    <option value="cat">Cat</option>
+</select>
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+</template>
+
+<style scoped>
+
+</style>
