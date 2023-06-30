03c09988bb3dcb233d31fe8e6a4969c75486af5a
Ruslan Makarov <ruslan.makarov@gmail.com>
2023-06-30 13:43:00 +0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <script setup>
+import { Button, Input, Select  } from "agnostic-vue";
 </script>
 
 <template>
-  <button>Button</button>
+  <Button>Go</Button>
 
-  <input type="text" value="Input" />
+  <Input id="1" v-model="message" size="small" placeholder="Enter name…" label="Small input" type="text" />
 
-  <select name="pets" id="pet-select">
-    <option value="">--Please choose an option--</option>
-    <option value="dog">Dog</option>
-    <option value="cat">Cat</option>
-</select>
+  <Select :options="[{ value: 'andre', label: 'Andre Agassi' }, { value: 'serena', label: 'Serena Williams'} , { value: 'mac', label: 'John McEnroe'}, { value: 'borg', label: 'Bjorn Borg'}, { value: 'althea', label: 'Althea Gibson'}, { value: 'roger', label: 'Roger Federer'}]" unique-id="sel1" name="select1" label-copy="Select the best tennis player of all time" />
+ 
 </template>
 
 <style scoped>
